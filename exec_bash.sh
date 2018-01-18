b8fbd18346e6a46a95873872f92418e5db060c52
Tsukasa Arima <pyar6329@gmail.com>
2018-01-18 15:25:54 +0900

    fix exec_bash

--- exec_bash.sh
+++ exec_bash.sh
@@ -3,21 +3,25 @@
 set -e
 
 readonly SCRIPT_DIR=$(echo $(cd $(dirname $0) && pwd))
+readonly DEPLOYMENT_NAME="debug-kubernetes"
+readonly DEPLOYMENT_FILE_NAME="deployment.yaml"
 
 count=0
 while :; do
   if [ "${count}" == "60" ]; then
-    [ "$(uname)" == "Darwin" ] && osascript -e 'display notification "progress 60s" with title "proris-script"'
+    [ "$(uname)" == "Darwin" ] && osascript -e 'display notification "progress 60s" with title "debug-deployment"'
     exit 1
   fi
 
-  UBUNTU_STATUS=$(kubectl get pod | grep ubuntu | awk '{print $3}')
-  [ -z "${UBUNTU_STATUS}" ] && kubectl create -f "${SCRIPT_DIR}/deployment.yml"
-  [ "${UBUNTU_STATUS}" = "Running" ] && break
+  ubuntu_status=$(kubectl get pod | grep "${DEPLOYMENT_NAME}" | awk '{print $3}')
+  [ -z "${ubuntu_status}" ] && kubectl create -f "${SCRIPT_DIR}/${DEPLOYMENT_FILE_NAME}"
+  [ "${ubuntu_status}" = "Running" ] && break
 
   count=$(expr $count + 1)
   sleep 1
 done
 
-kubectl exec -it ubuntu bash
-kubectl delete -f "${SCRIPT_DIR}/deployment.yml"
+pod_name=$(kubectl get pod | grep "${DEPLOYMENT_NAME}" | awk '{print $1}')
+
+kubectl exec -it "${pod_name}" bash
+kubectl delete -f "${SCRIPT_DIR}/${DEPLOYMENT_FILE_NAME}"
